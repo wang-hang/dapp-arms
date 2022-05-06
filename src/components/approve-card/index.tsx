34c2c1d3e0da84124153cc37880c2ef54dca11e5
wanghang <wanghang@hnnewsoft.com>
2022-05-06 19:22:11 +0800

    feat: 完成授权卡片

--- src/components/approve-card/index.tsx
+++ src/components/approve-card/index.tsx
@@ -1,28 +1,78 @@
 import { useState } from 'react';
 import useApprove, { useApproveStatus } from '@/hooks/useApprove';
-import { Card, Input, Space, Button, Row } from 'antd';
+import { Card, Input, Space, Button, Row, message } from 'antd';
+import { weiToEther, etherToWei } from '@/utils/unit';
 
 export default function ApproveCard() {
   const [tokenAddress, setTokenAddress] = useState('');
   const [approveValue, setApproveValue] = useState('');
   const [beApprovedAddress, setBeApprovedAddress] = useState('');
-  const { isApproved } = useApproveStatus(tokenAddress, beApprovedAddress);
-  const { approve, loading } = useApprove();
-  function toApprove() {}
+  const { isApproved, allowance } = useApproveStatus(
+    tokenAddress,
+    beApprovedAddress,
+  );
+  const [approveLoading, setApproveLoading] = useState(false);
+  const [cancelApproveLoading, setCancelApproveLoading] = useState(false);
+  const { approve } = useApprove();
+
+  function toApprove() {
+    if (!validate()) return;
+    setApproveLoading(true);
+    approve(tokenAddress, beApprovedAddress)
+      .then(() => {
+        message.success('授权成功');
+      })
+      .finally(() => setApproveLoading(false));
+  }
 
   function cancelApprove() {
-    approve(tokenAddress, beApprovedAddress, '0');
+    if (!validate()) return;
+    setCancelApproveLoading(true);
+    approve(tokenAddress, beApprovedAddress, '0')
+      .then(() => {
+        message.success('取消授权成功');
+      })
+      .finally(() => setCancelApproveLoading(false));
+  }
+
+  function validate() {
+    if (!tokenAddress) {
+      message.error('请输入取消授权的代币地址');
+      return false;
+    }
+    if (!beApprovedAddress) {
+      message.error('请输入被取消授权的合约地址');
+      return false;
+    }
+    return true;
   }
   return (
     <Card title="合约授权">
       <Row>
-        <Input placeholder="请输入授权额度"></Input>
-        <Input placeholder="请输入被授权的地址"></Input>
-        <Input placeholder="请输入授权的代币的地址"></Input>
+        <Input
+          placeholder="请输入授权额度"
+          value={approveValue}
+          onChange={(v) => setApproveValue(v.target.value)}
+        ></Input>
+        <Input
+          placeholder="请输入被授权的地址"
+          value={tokenAddress}
+          onChange={(v) => setBeApprovedAddress(v.target.value)}
+        ></Input>
+        <Input
+          placeholder="请输入授权的代币的地址"
+          value={beApprovedAddress}
+          onChange={(e) => setTokenAddress(e.target.value)}
+        ></Input>
       </Row>
       <Row>
-        <Button loading={loading}>授权</Button>
-        <Button loading={loading} onClick={cancelApprove}>
+        <p>授权额度：{weiToEther(allowance).toString()}</p>
+      </Row>
+      <Row>
+        <Button loading={approveLoading} onClick={toApprove}>
+          授权
+        </Button>
+        <Button loading={cancelApproveLoading} onClick={cancelApprove}>
           取消授权
         </Button>
       </Row>
