329936c5e615a880debf30fe4f881a39fb006321
wanghang <wanghang@hnnewsoft.com>
2022-06-11 21:16:49 +0800

    chore: 调整打包配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'umi';
 
+const repoName = 'dapp-arms';
+
+const isProd = process.env.NODE_ENV === 'production';
+console.log('isPROD: ', isProd, process.env.NODE_ENV);
 export default defineConfig({
+  // base: repoName,
   nodeModulesTransform: {
     type: 'none',
   },
@@ -8,6 +13,9 @@ export default defineConfig({
   mfsu: {},
   hash: true,
   esbuild: {},
+  history: {
+    type: 'hash',
+  },
   outputPath: 'docs',
-  publicPath: 'https://wang-hang.github.io/dapp-arms/',
+  publicPath: isProd ? `https://wang-hang.github.io/${repoName}/` : undefined,
 });
